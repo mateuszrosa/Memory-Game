af9618a224e3018b0fbe596bc8f39fa02633ca0c
mateuszrosa <mateusz.rosa@gmail.com>
2019-05-08 20:04:42 +0200

    checking pair functions

--- js/app.js
+++ js/app.js
@@ -1,9 +1,39 @@
 const cards = document.querySelectorAll('.game-card');
-console.log(cards);
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 const flipCard = function() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    };
+    secondCard = this;
+    hasFlippedCard = false;
+
+    checkForMatch();
+};
+
+const checkForMatch = () => {
+    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+    isMatch ? disableCards() : unFlipCards();
+};
+
+const disableCards = () => {
+    console.log('disable');
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+};
+
+const unFlipCards = () => {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        console.log('unflip');
+    }, 1500)
 };
 
 cards.forEach(card => {
     card.addEventListener('click', flipCard);
-})
+});
