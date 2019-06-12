90b93b7ef16bcbb7f6e83c3b82671c25c308fc7e
mateuszrosa <mateusz.rosa@gmail.com>
2019-06-12 13:02:21 +0200

    shuffling cards

--- js/app1.js
+++ js/app1.js
@@ -1,14 +1,16 @@
 class MemoryGame {
     constructor() {
-        document.querySelectorAll('.game-card').forEach(card => {
-            card.addEventListener('click', this.flipCard.bind(this));
-        }),
+        this.cards = document.querySelectorAll('.game-card'),
+            this.cards.forEach(card => {
+                card.addEventListener('click', this.flipCard.bind(this));
+            }),
             this.h1 = document.querySelector('h1'),
             this.btn = document.querySelector('button'),
             this.hasFlippedCard = false,
             this.lockBoard = false,
             this.firstCard,
-            this.secondCard
+            this.secondCard,
+            this.shuffle();
     }
     flipCard(e) {
         if (this.lockBoard) return;
@@ -48,6 +50,12 @@ class MemoryGame {
     resetBoard() {
         [this.hasFlippedCard, this.lockBoard] = [false, false];
         [this.firstCard, this.secondCard] = [null, null];
+    }
+    shuffle() {
+        this.cards.forEach(card => {
+            let random = Math.floor(Math.random() * 12);
+            card.style.order = random;
+        })
     }
 };
 
